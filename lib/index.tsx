ca9c73d9f491404d6f92808626e38bd4f17bcdde
Oleg Prokopyev <arkkeeper@yandex.ru>
2024-07-18 15:25:08 +0100

    gitignore

---
+++ lib/index.tsx
@@ -0,0 +1,7 @@
+import DatePicker from './components/DatePicker';
+
+export function multiply(a: number, b: number): Promise<number> {
+  return Promise.resolve(a * b);
+}
+
+export default DatePicker;
